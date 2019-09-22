93f42d33b87a059fb11e55bbc1bec52449705d59
dukedd <duke_dd@163.com>
2019-09-22 10:32:51 +0800

    模块 HelloWorld 的初始签入(from MAC)

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String strName;
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("What's your name?");
+            strName = Console.ReadLine();
+            foreach (var item in strName)
+            {
+            }
+            Console.WriteLine("Hello, my friend " + strName);
+            Console.WriteLine("What's your name?");
+            strName = Console.ReadLine();
+            foreach (var item in strName)
+            {
+            }
+            Console.WriteLine("Hello, my friend " + strName);
+        }
+    }
+}
